3b22019b4581f28d008307d6c8a0216298604663
axa25 <emevig@gmail.com>
2019-08-29 19:13:21 +0200

    Updates

---
+++ precache-manifest.14772e071c809dd20c8b8153d70d2244.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1a879a32f4dd4ad28e7e76401286ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bb9198d8bdb1fda4606",
+    "url": "/static/js/2.66f8a939.chunk.js"
+  },
+  {
+    "revision": "a7264a82045429f63725",
+    "url": "/static/js/main.a083302c.chunk.js"
+  },
+  {
+    "revision": "cb0c4953a2122aa87834",
+    "url": "/static/js/runtime~main.9c97f219.js"
+  }
+]);
