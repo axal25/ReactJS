7e1432818fbfb8ece86a52d559f8ec61306effb3
axa25 <emevig@gmail.com>
2019-09-01 20:51:08 +0200

    Updates

---
+++ precache-manifest.38e5dff5f080959f459fee3e41605e0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44fec7b41f2f8db943eb96227e019ea",
+    "url": "/ReactJS/index.html"
+  },
+  {
+    "revision": "0a49dd69b24384d7dd22",
+    "url": "/ReactJS/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1aab3137e99634d41556",
+    "url": "/ReactJS/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0a49dd69b24384d7dd22",
+    "url": "/ReactJS/static/js/2.910f0d66.chunk.js"
+  },
+  {
+    "revision": "1aab3137e99634d41556",
+    "url": "/ReactJS/static/js/main.706e02d5.chunk.js"
+  },
+  {
+    "revision": "282ac1c1ebee030f7921",
+    "url": "/ReactJS/static/js/runtime~main.a59636c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactJS/static/media/logo.5d5d9eef.svg"
+  }
+]);
